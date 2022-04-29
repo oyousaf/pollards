fe361b67fdded94803a3517abc25ab0e246141e5
Omar ♔ <o_yousaf@live.co.uk>
2022-04-29 18:54:52 +0100

    pollards

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,41 +1,8 @@
 import React from 'react'
 import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
-import img4 from '../assets/images/4.png'
-import img5 from '../assets/images/5.png'
-import img6 from '../assets/images/6.png'
-import img7 from '../assets/images/7.png'
-import img8 from '../assets/images/8.png'
-import img9 from '../assets/images/9.png'
-import img10 from '../assets/images/10.png'
-import img11 from '../assets/images/11.png'
-import img12 from '../assets/images/12.png'
-import img13 from '../assets/images/13.png'
-import img14 from '../assets/images/14.png'
-import img15 from '../assets/images/15.png'
-import img16 from '../assets/images/16.png'
-import img17 from '../assets/images/17.png'
-import img18 from '../assets/images/18.png'
-import img19 from '../assets/images/19.png'
-import img20 from '../assets/images/20.png'
-import img21 from '../assets/images/21.png'
-import img22 from '../assets/images/22.png'
-import img23 from '../assets/images/23.png'
-import img24 from '../assets/images/24.png'
-import img25 from '../assets/images/25.png'
-import img26 from '../assets/images/26.png'
-import img27 from '../assets/images/27.png'
-import img28 from '../assets/images/28.png'
-import img29 from '../assets/images/29.png'
-import img30 from '../assets/images/30.png'
-import img31 from '../assets/images/31.png'
-import img32 from '../assets/images/32.png'
-import img33 from '../assets/images/33.png'
-import img34 from '../assets/images/34.png'
-import img35 from '../assets/images/35.png'
-import img36 from '../assets/images/36.png'
-import img37 from '../assets/images/37.png'
-import img38 from '../assets/images/38.png'
+
+import { images } from '../constants/'
 
 
 const Gallery = () => {
@@ -55,109 +22,109 @@ const Gallery = () => {
             <div className="embla" ref={emblaRef}>
                 <div className="embla__container">
                 <div className="embla__slide">
-                    <img src={ img4 } alt='pollards'/>
+                    <img src={ images.img4 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img5 } alt='pollards'/>
+                    <img src={ images.img5 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img6 } alt='pollards'/>
+                    <img src={ images.img6 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img7 } alt='pollards'/>
+                    <img src={ images.img7 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img8 } alt='pollards'/>
+                    <img src={ images.img8 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img9 } alt='pollards'/>
+                    <img src={ images.img9 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img10 } alt='pollards'/>
+                    <img src={ images.img10 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img11 } alt='pollards'/>
+                    <img src={ images.img11 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img12 } alt='pollards'/>
+                    <img src={ images.img12 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img13 } alt='pollards'/>
+                    <img src={ images.img13 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img14 } alt='pollards'/>
+                    <img src={ images.img14 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img15 } alt='pollards'/>
+                    <img src={ images.img15 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img16 } alt='pollards'/>
+                    <img src={ images.img16 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img17 } alt='pollards'/>
+                    <img src={ images.img17 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img18 } alt='pollards'/>
+                    <img src={ images.img18 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img19 } alt='pollards'/>
+                    <img src={ images.img19 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img20 } alt='pollards'/>
+                    <img src={ images.img20 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img21 } alt='pollards'/>
+                    <img src={ images.img21 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img22 } alt='pollards'/>
+                    <img src={ images.img22 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img23 } alt='pollards'/>
+                    <img src={ images.img23 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img24 } alt='pollards'/>
+                    <img src={ images.img24 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img25 } alt='pollards'/>
+                    <img src={ images.img25 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img26 } alt='pollards'/>
+                    <img src={ images.img26 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img27 } alt='pollards'/>
+                    <img src={ images.img27 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img28 } alt='pollards'/>
+                    <img src={ images.img28 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img29 } alt='pollards'/>
+                    <img src={ images.img29 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img30 } alt='pollards'/>
+                    <img src={ images.img30 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img31 } alt='pollards'/>
+                    <img src={ images.img31 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img32 } alt='pollards'/>
+                    <img src={ images.img32 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img33 } alt='pollards'/>
+                    <img src={ images.img33 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img34 } alt='pollards'/>
+                    <img src={ images.img34 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img35 } alt='pollards'/>
+                    <img src={ images.img35 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img36 } alt='pollards'/>
+                    <img src={ images.img36 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img37 } alt='pollards'/>
+                    <img src={ images.img37 } alt='pollards'/>
                 </div>
                 <div className="embla__slide">
-                    <img src={ img38 } alt='pollards'/>
+                    <img src={ images.img38 } alt='pollards'/>
                 </div>
                 </div>
             </div>     
---
+++ src/constants/images.jsx
@@ -0,0 +1,73 @@
+import img4 from '../assets/images/4.png'
+import img5 from '../assets/images/5.png'
+import img6 from '../assets/images/6.png'
+import img7 from '../assets/images/7.png'
+import img8 from '../assets/images/8.png'
+import img9 from '../assets/images/9.png'
+import img10 from '../assets/images/10.png'
+import img11 from '../assets/images/11.png'
+import img12 from '../assets/images/12.png'
+import img13 from '../assets/images/13.png'
+import img14 from '../assets/images/14.png'
+import img15 from '../assets/images/15.png'
+import img16 from '../assets/images/16.png'
+import img17 from '../assets/images/17.png'
+import img18 from '../assets/images/18.png'
+import img19 from '../assets/images/19.png'
+import img20 from '../assets/images/20.png'
+import img21 from '../assets/images/21.png'
+import img22 from '../assets/images/22.png'
+import img23 from '../assets/images/23.png'
+import img24 from '../assets/images/24.png'
+import img25 from '../assets/images/25.png'
+import img26 from '../assets/images/26.png'
+import img27 from '../assets/images/27.png'
+import img28 from '../assets/images/28.png'
+import img29 from '../assets/images/29.png'
+import img30 from '../assets/images/30.png'
+import img31 from '../assets/images/31.png'
+import img32 from '../assets/images/32.png'
+import img33 from '../assets/images/33.png'
+import img34 from '../assets/images/34.png'
+import img35 from '../assets/images/35.png'
+import img36 from '../assets/images/36.png'
+import img37 from '../assets/images/37.png'
+import img38 from '../assets/images/38.png'
+
+export default {
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18,
+    img19,
+    img20,
+    img21,
+    img22,
+    img23,
+    img24,
+    img25,
+    img26,
+    img27,
+    img28,
+    img29,
+    img30,
+    img31,
+    img32,
+    img33,
+    img34,
+    img35,
+    img36,
+    img37,
+    img38,
+};
