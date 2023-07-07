672c2c8c854472dda14a66b6ac442efb5743618b
Omar ♔ <o_yousaf@live.co.uk>
2023-07-07 12:19:48 +0100

    pollards

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -3,22 +3,23 @@ import styles from "../style";
 import { BsArrowDownShort } from "react-icons/bs";
 
 const Explore = () => (
-  <div
-    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-pink-gradient p-[2px] cursor-pointer`}
-  >
+  <a href="#about">
     <div
-      className={`${styles.flexCenter} flex-col bg-[#C3B1E1] w-[100%] h-[100%] rounded-full`}
+      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-pink-gradient p-[2px] cursor-pointer`}
     >
-      <div className={`${styles.flexStart} flex-row`}>
-        <p className="font-poppins font-medium text-[18px] leading-[23px]">
-          <span className="text-gray-100 text-center">Explore</span>
-          <a href="#about">
+      <div
+        className={`${styles.flexCenter} flex-col bg-[#C3B1E1] w-[100%] h-[100%] rounded-full`}
+      >
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className="font-poppins font-medium text-[18px] leading-[23px]">
+            <span className="text-gray-100 text-center">Explore</span>
+
             <BsArrowDownShort className="ml-3 w-[40px] h-[40px] animate-bounce text-primary hover:text-white" />
-          </a>
-        </p>
+          </p>
+        </div>
       </div>
     </div>
-  </div>
+  </a>
 );
 
 export default Explore;
