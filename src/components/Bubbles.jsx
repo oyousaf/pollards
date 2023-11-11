0eded4f82c9a9372e3fc3078ee25800461d1322b
Omar ♔ <o_yousaf@live.co.uk>
2023-11-11 15:45:06 +0000

    adding bubbles

--- src/components/Bubbles.jsx
+++ src/components/Bubbles.jsx
@@ -5,7 +5,7 @@ import { useHover } from "react-use-gesture";
 import { team } from "../constants";
 
 const Bubbles = () => {
-  const [springProps, set] = useSpring(() => ({
+  const [springProps, set, stop] = useSpring(() => ({
     x: Math.random() * window.innerWidth,
     y: Math.random() * window.innerHeight,
   }));
@@ -17,19 +17,28 @@ const Bubbles = () => {
     });
   });
 
+  React.useEffect(() => {
+    return stop;
+  }, [stop]);
+
   return (
-    <div
-      className="absolute left-[calc(var(--x)/16)] top-[calc(var(--y)/16)] cursor-pointer"
-      {...bind()}
-    >
-      <div className="p-4 bg-black bg-opacity-80 rounded-md">
-        <img
-          src={team[0].image}
-          alt="Bubble"
-          className="w-12 h-12 rounded-full"
-        />
-        <p className="text-white">{team[0].name}</p>
-      </div>
+    <div>
+      {team.map((member, index) => (
+        <animated.div
+          key={index}
+          className="left-[calc(var(--x)/16)] top-[calc(var(--y)/16)] cursor-pointer"
+          {...bind()}
+        >
+          <div className="p-4 bg-black bg-opacity-80 rounded-md">
+            <img
+              src={member.image}
+              alt="Bubble"
+              className="w-12 h-12 rounded-full"
+            />
+            <p className="text-white">{member.name}</p>
+          </div>
+        </animated.div>
+      ))}
     </div>
   );
 };
