51865a34be2ac4566ea4f96c13a8343d09d0c1c1
Omar ♔ <o_yousaf@live.co.uk>
2023-11-18 18:43:48 +0000

    squashing bugs

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -13,11 +13,15 @@ const NavLink = ({ to, onClick, children }) => {
     }
   };
 
+  const isActive = location.pathname === to;
+
   return (
     <RouterNavLink
       to={to}
       onClick={handleClick}
-      className="font-poppins font-medium cursor-pointer text-[20px] text-gray-300 hover:text-gray-100"
+      className={`font-poppins font-medium cursor-pointer text-[20px] ${
+        isActive ? "text-[#C3B1E1]" : "text-gray-300"
+      } hover:text-gray-100`}
     >
       {children}
     </RouterNavLink>
