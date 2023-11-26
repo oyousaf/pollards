1853cb4f98b0328b98756b31c3254c708607cdc2
Omar ♔ <o_yousaf@live.co.uk>
2023-11-26 17:22:37 +0000

    squashing bugs

---
+++ src/reportWebVitals.js
@@ -0,0 +1,20 @@
+import { getCLS, getFID, getLCP, getFCP, getTTFB } from 'web-vitals';
+
+const sendToAnalytics = ({ id, name, value }) => {
+  window.gtag('event', name, {
+    event_category: 'Web Vitals',
+    event_label: id,
+    value: Math.round(name === 'CLS' ? value * 1000 : value),
+    non_interaction: true,
+  });
+};
+
+export function reportWebVitals(onPerfEntry) {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getLCP(onPerfEntry);
+    getFCP(onPerfEntry);
+    getTTFB(onPerfEntry);
+  }
+}
