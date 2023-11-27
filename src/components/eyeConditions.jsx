851d1729d0fe96efc71cbaffdf584b0a6d4f82a4
Omar ♔ <o_yousaf@live.co.uk>
2023-11-27 16:13:50 +0000

    reworking info component

--- src/components/eyeConditions.jsx
+++ src/components/eyeConditions.jsx
@@ -1,111 +1,138 @@
 const eyeConditions = [
-    {
-      title: "Age-related Macular Degeneration (AMD)",
-      description: "(AMD) is a form of macular dystrophy. ...",
-    },
-    {
-      title: "Amblyopia (Lazy Eye)",
-      description: "Amblyopia, often known as lazy eye, is a condition where one eye has reduced vision due to abnormal visual development during childhood. It can result in a weaker eye that the brain may ignore.",
-    },
-    {
-      title: "Astigmatism",
-      description: "Astigmatism is a refractive error caused by an irregular shape of the cornea or lens, leading to blurred or distorted vision. It can coexist with nearsightedness or farsightedness.",
-    },
-    {
-      title: "Blepharitis",
-      description: "Blepharitis is an inflammation of the eyelids, often caused by bacterial or skin conditions. It can result in redness, itching, and the formation of crusts on the eyelashes.",
-    },
-    {
-      title: "Birdshot Uveitis",
-      description: "Birdshot Chorioretinopathy is a rare, progressive inflammation of the iris, choroid and cilary body, which together make up a part of the eye known as the uvea. If left undiagnosed or untreated, the condition can cause blindness.",
-    },
-    {
-      title: "Cataracts",
-      description: "Cataracts are a common eye condition in which the lens inside the eye gradually becomes less transparent with age. Over time, a cataract can become worse, making vision cloudier, but it is usually treatable with a straightforward operation.",
-    },
-    {
-      title: "Charles Bonnet Syndrome",
-      description: "Charles Bonnet Syndrome (CBS) is a condition which causes vivid, silent, visual hallucinations. It can develop in a person of any age – children are not exempt – when over 60% of sight is lost. Not everyone who loses sight develops CBS.",
-    },
-    {
-      title: "Color Blindness",
-      description: "Color blindness is a genetic condition that affects a person's ability to perceive certain colors accurately. The most common type is red-green color blindness, where individuals may have difficulty distinguishing between red and green hues.",
-    },
-    {
-      title: "Conjunctivitis (Pink Eye)",
-      description: "Conjunctivitis, commonly known as pink eye, is an inflammation of the conjunctiva—the clear tissue covering the white part of the eye. Symptoms include redness, itching, and discharge, and it can be caused by infections, allergies, or irritants.",
-    },
-    {
-      title: "Corneal Abrasion",
-      description: "A corneal abrasion is a scratch on the cornea, the clear front part of the eye. It can cause pain, redness, and sensitivity to light.",
-    },
-    {
-      title: "Diabetic Retinopathy",
-      description: "Diabetic Retinopathy is a complication of diabetes that damages the tiny blood vessels that deliver blood to the retinas. The condition can cause blindness if left undiagnosed and untreated.",
-    },
-    {
-      title: "Dry Eye Syndrome",
-      description: "Dry eye syndrome occurs when the eyes do not produce enough tears or when tears evaporate too quickly. This can result in discomfort, a gritty sensation, and even blurred vision.",
-    },
-    {
-      title: "Glaucoma",
-      description: "Glaucoma is the name given to a group of eye conditions that cause sight loss by damaging your optic nerve. Sight loss caused by glaucoma is permanent but there are methods of prevention.",
-    },
-    {
-      title: "Hemianopia",
-      description: "Hemianopia can be caused by brain trauma, the most common of which is related to stroke. The extent of sight loss varies from person to person and depends on the area of the brain that has been affected by the stroke. Around two-thirds of people who have a stroke experience some form of vision loss or difficulty in seeing, including Hemianopia.",
-    },
-    {
-      title: "Keratoconus",
-      description: "Keratoconus is a progressive eye condition where the normally dome-shaped cornea thins and begins to bulge into a cone-like shape. This distortion deflects light as it enters the eye on the way to the retina, which causes distorted vision.",
-    },
-    {
-      title: "Macular Edema",
-      description: "Macular edema is a condition characterized by the accumulation of fluid in the macula, the central part of the retina. This can lead to distorted or blurred central vision, impacting activities like reading and recognizing faces.",
-    },
-    {
-      title: "Night Blindness",
-      description: "Night blindness, or nyctalopia, is a condition where an individual has difficulty seeing in low-light conditions. It can be caused by various factors, including vitamin A deficiency or certain genetic conditions.",
-    },
-    {
-      title: "Nystagmus",
-      description: "Nystagmus is a condition characterized by involuntary and repetitive eye movements. These movements can affect vision, causing reduced visual acuity and depth perception.",
-    },
-    {
-      title: "Ocular Hypertension",
-      description: "Ocular hypertension refers to elevated intraocular pressure in the eyes. While not necessarily a disease, it can be a risk factor for the development of glaucoma, a serious eye condition.",
-    },
-    {
-      title: "Ocular Migraines",
-      description: "Ocular migraines are a type of migraine that involves visual disturbances, such as flashing lights or temporary vision loss. These symptoms typically resolve on their own.",
-    },
-    {
-      title: "Pinguecula and Pterygium",
-      description: "Pinguecula and pterygium are growths on the conjunctiva. Pinguecula is a yellowish deposit, while pterygium is a more significant growth that can extend onto the cornea, affecting vision.",
-    },
-    {
-      title: "Presbyopia",
-      description: "Presbyopia is a common age-related condition where the eye's lens loses its flexibility, making it difficult to focus on close objects. It typically becomes noticeable after the age of 40.",
-    },
-    {
-      title: "Ptosis",
-      description: "Ptosis is the drooping of the upper eyelid, which can partially or completely cover the eye. It may be congenital (present at birth) or acquired due to aging, injury, or other factors. Ptosis can impact both the appearance and function of the eye.",
-    },
-    {
-      title: "Retinal Detachment",
-      description: "Retinal detachment occurs when the retina separates from the back of the eye. This is a medical emergency, and symptoms may include sudden flashes of light, floaters, and a curtain-like shadow over the visual field.",
-    },
-    {
-      title: "Retinitis Pigmentosa",
-      description: "Retinitis Pigmentosa (RP) is the name given to a group of inherited eye conditions called retinal dystrophies. These conditions cause a slow loss of vision, beginning with night vision and peripheral (side) vision and eventually affecting central, color, and reading vision.",
-    },
-    {
-      title: "Strabismus",
-      description: "Strabismus is a condition in which the eyes do not align properly. This misalignment can lead to double vision and may affect depth perception. Strabismus is commonly referred to as 'crossed eyes' or 'lazy eye.'",
-    },
-    {
-      title: "Uveitis",
-      description: "Uveitis is an inflammation of the uvea, the middle layer of the eye. It can cause eye redness, pain, light sensitivity, and may lead to complications affecting vision if left untreated.",
-    },
-  ];
-  
+  {
+    title: "Age-related Macular Degeneration (AMD)",
+    description: "(AMD) is a form of macular dystrophy. ...",
+  },
+  {
+    title: "Amblyopia (Lazy Eye)",
+    description:
+      "Amblyopia, often known as lazy eye, is a condition where one eye has reduced vision due to abnormal visual development during childhood. It can result in a weaker eye that the brain may ignore.",
+  },
+  {
+    title: "Astigmatism",
+    description:
+      "Astigmatism is a refractive error caused by an irregular shape of the cornea or lens, leading to blurred or distorted vision. It can coexist with nearsightedness or farsightedness.",
+  },
+  {
+    title: "Blepharitis",
+    description:
+      "Blepharitis is an inflammation of the eyelids, often caused by bacterial or skin conditions. It can result in redness, itching, and the formation of crusts on the eyelashes.",
+  },
+  {
+    title: "Birdshot Uveitis",
+    description:
+      "Birdshot Chorioretinopathy is a rare, progressive inflammation of the iris, choroid and cilary body, which together make up a part of the eye known as the uvea. If left undiagnosed or untreated, the condition can cause blindness.",
+  },
+  {
+    title: "Cataracts",
+    description:
+      "Cataracts are a common eye condition in which the lens inside the eye gradually becomes less transparent with age. Over time, a cataract can become worse, making vision cloudier, but it is usually treatable with a straightforward operation.",
+  },
+  {
+    title: "Charles Bonnet Syndrome",
+    description:
+      "Charles Bonnet Syndrome (CBS) is a condition which causes vivid, silent, visual hallucinations. It can develop in a person of any age – children are not exempt – when over 60% of sight is lost. Not everyone who loses sight develops CBS.",
+  },
+  {
+    title: "Color Blindness",
+    description:
+      "Color blindness is a genetic condition that affects a person's ability to perceive certain colors accurately. The most common type is red-green color blindness, where individuals may have difficulty distinguishing between red and green hues.",
+  },
+  {
+    title: "Conjunctivitis (Pink Eye)",
+    description:
+      "Conjunctivitis, commonly known as pink eye, is an inflammation of the conjunctiva—the clear tissue covering the white part of the eye. Symptoms include redness, itching, and discharge, and it can be caused by infections, allergies, or irritants.",
+  },
+  {
+    title: "Corneal Abrasion",
+    description:
+      "A corneal abrasion is a scratch on the cornea, the clear front part of the eye. It can cause pain, redness, and sensitivity to light.",
+  },
+  {
+    title: "Diabetic Retinopathy",
+    description:
+      "Diabetic Retinopathy is a complication of diabetes that damages the tiny blood vessels that deliver blood to the retinas. The condition can cause blindness if left undiagnosed and untreated.",
+  },
+  {
+    title: "Dry Eye Syndrome",
+    description:
+      "Dry eye syndrome occurs when the eyes do not produce enough tears or when tears evaporate too quickly. This can result in discomfort, a gritty sensation, and even blurred vision.",
+  },
+  {
+    title: "Glaucoma",
+    description:
+      "Glaucoma is the name given to a group of eye conditions that cause sight loss by damaging your optic nerve. Sight loss caused by glaucoma is permanent but there are methods of prevention.",
+  },
+  {
+    title: "Hemianopia",
+    description:
+      "Hemianopia can be caused by brain trauma, the most common of which is related to stroke. The extent of sight loss varies from person to person and depends on the area of the brain that has been affected by the stroke. Around two-thirds of people who have a stroke experience some form of vision loss or difficulty in seeing, including Hemianopia.",
+  },
+  {
+    title: "Keratoconus",
+    description:
+      "Keratoconus is a progressive eye condition where the normally dome-shaped cornea thins and begins to bulge into a cone-like shape. This distortion deflects light as it enters the eye on the way to the retina, which causes distorted vision.",
+  },
+  {
+    title: "Macular Edema",
+    description:
+      "Macular edema is a condition characterized by the accumulation of fluid in the macula, the central part of the retina. This can lead to distorted or blurred central vision, impacting activities like reading and recognizing faces.",
+  },
+  {
+    title: "Night Blindness",
+    description:
+      "Night blindness, or nyctalopia, is a condition where an individual has difficulty seeing in low-light conditions. It can be caused by various factors, including vitamin A deficiency or certain genetic conditions.",
+  },
+  {
+    title: "Nystagmus",
+    description:
+      "Nystagmus is a condition characterized by involuntary and repetitive eye movements. These movements can affect vision, causing reduced visual acuity and depth perception.",
+  },
+  {
+    title: "Ocular Hypertension",
+    description:
+      "Ocular hypertension refers to elevated intraocular pressure in the eyes. While not necessarily a disease, it can be a risk factor for the development of glaucoma, a serious eye condition.",
+  },
+  {
+    title: "Ocular Migraines",
+    description:
+      "Ocular migraines are a type of migraine that involves visual disturbances, such as flashing lights or temporary vision loss. These symptoms typically resolve on their own.",
+  },
+  {
+    title: "Pinguecula and Pterygium",
+    description:
+      "Pinguecula and pterygium are growths on the conjunctiva. Pinguecula is a yellowish deposit, while pterygium is a more significant growth that can extend onto the cornea, affecting vision.",
+  },
+  {
+    title: "Presbyopia",
+    description:
+      "Presbyopia is a common age-related condition where the eye's lens loses its flexibility, making it difficult to focus on close objects. It typically becomes noticeable after the age of 40.",
+  },
+  {
+    title: "Ptosis",
+    description:
+      "Ptosis is the drooping of the upper eyelid, which can partially or completely cover the eye. It may be congenital (present at birth) or acquired due to aging, injury, or other factors. Ptosis can impact both the appearance and function of the eye.",
+  },
+  {
+    title: "Retinal Detachment",
+    description:
+      "Retinal detachment occurs when the retina separates from the back of the eye. This is a medical emergency, and symptoms may include sudden flashes of light, floaters, and a curtain-like shadow over the visual field.",
+  },
+  {
+    title: "Retinitis Pigmentosa",
+    description:
+      "Retinitis Pigmentosa (RP) is the name given to a group of inherited eye conditions called retinal dystrophies. These conditions cause a slow loss of vision, beginning with night vision and peripheral (side) vision and eventually affecting central, color, and reading vision.",
+  },
+  {
+    title: "Strabismus",
+    description:
+      "Strabismus is a condition in which the eyes do not align properly. This misalignment can lead to double vision and may affect depth perception. Strabismus is commonly referred to as 'crossed eyes' or 'lazy eye.'",
+  },
+  {
+    title: "Uveitis",
+    description:
+      "Uveitis is an inflammation of the uvea, the middle layer of the eye. It can cause eye redness, pain, light sensitivity, and may lead to complications affecting vision if left untreated.",
+  },
+];
+
+export default eyeConditions;
