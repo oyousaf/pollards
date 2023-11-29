8d29dc1d92490c1465f9f2545336c21a3fe1af85
Omar ♔ <o_yousaf@live.co.uk>
2023-11-29 12:26:43 +0000

    squashing bugs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,77 +7,81 @@ import socialMedia from "./SocialMedia";
 import Gmap from "./Gmap";
 import { FaArrowUp } from "react-icons/fa";
 
-const Footer = () => {};
-const [showScroll, setShowScroll] = useState(false);
+const Footer = () => {
+  const [showScroll, setShowScroll] = useState(false);
 
-const checkScrollTop = () => {
-  if (!showScroll && windowpageYOffset > 400) {
-    setShowScroll(true);
-  } else if (showScroll && window.windowpageYOffset <= 400) {
-    setShowScroll(false);
-  }
-};
-
-const scrollToTop = () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-};
-
-useEffect(() => {
-  window.addEventListener("scroll", checkScrollTop);
-  return () => {
-    window.removeEventListener("scroll", checkScrollTop);
+  const checkScrollTop = () => {
+    if (!showScroll && windowpageYOffset > 400) {
+      setShowScroll(true);
+    } else if (showScroll && window.windowpageYOffset <= 400) {
+      setShowScroll(false);
+    }
   };
-}, [showScroll]);
 
-return (
-  <section
-    id="contact"
-    className={`${styles.flexCenter} ${styles.paddingY} flex-col font-poppins border-t-[1px] border-t-[#c3b1e1]`}
-  >
-    <Gmap />
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
-    <div className={`${styles.flexStart} mb-8 pt-6 w-full`}>
-      <div className="flex-1 flex flex-col justify-center items-center">
-        <a href="https://pollards.info">
-          <img
-            src={logo}
-            alt="pollards"
-            className="w-[50px] h-[50px] object-contain"
-          />
-        </a>
-        <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
-          Connect with us
-        </p>
-        <div className="w-full flex flex-col justify-between items-center pt-6">
-          <p className="flex flex-row">
-            {socialMedia.map((social, index) => (
-              <a
-                key={social.id}
-                href={social.link}
-                target={social.target}
-                className={`text-3xl text-gray-300 hover:text-gray-100 active:text-[#C3B1E1] object-contain cursor-pointer ${
-                  index !== socialMedia.length - 1 ? "mr-6" : ""
-                }`}
-              >
-                {social.icon}
-              </a>
-            ))}
+  useEffect(() => {
+    window.addEventListener("scroll", checkScrollTop);
+    return () => {
+      window.removeEventListener("scroll", checkScrollTop);
+    };
+  }, [showScroll]);
+
+  return (
+    <section
+      id="contact"
+      className={`${styles.flexCenter} ${styles.paddingY} flex-col font-poppins border-t-[1px] border-t-[#c3b1e1]`}
+    >
+      <Gmap />
+
+      <div className={`${styles.flexStart} mb-8 pt-6 w-full`}>
+        <div className="flex-1 flex flex-col justify-center items-center">
+          <a href="https://pollards.info">
+            <img
+              src={logo}
+              alt="pollards"
+              className="w-[50px] h-[50px] object-contain"
+            />
+          </a>
+          <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
+            Connect with us
           </p>
-        </div>
-        <div className="w-full flex flex-col justify-between items-center pt-6">
-          <p className="font-medium text-[20px] leading-[27px] text-white pt-6">
-            &copy; {new Date().getFullYear()} Pollards Opticians
-          </p>
-        </div>
+          <div className="w-full flex flex-col justify-between items-center pt-6">
+            <p className="flex flex-row">
+              {socialMedia.map((social, index) => (
+                <a
+                  key={social.id}
+                  href={social.link}
+                  target={social.target}
+                  className={`text-3xl text-gray-300 hover:text-gray-100 active:text-[#C3B1E1] object-contain cursor-pointer ${
+                    index !== socialMedia.length - 1 ? "mr-6" : ""
+                  }`}
+                >
+                  {social.icon}
+                </a>
+              ))}
+            </p>
+          </div>
+          <div className="w-full flex flex-col justify-between items-center pt-6">
+            <p className="font-medium text-[20px] leading-[27px] text-white pt-6">
+              &copy; {new Date().getFullYear()} Pollards Opticians
+            </p>
+          </div>
 
-        {showScroll && (
-          <button
-            className={`${styles.scrollButton} bg-[#C3B1E1] text-white p-2 rounded-full fixed bottom-7 right-7`}
-          ></button>
-        )}
+          {showScroll && (
+            <button
+              className={`${styles.scrollButton} bg-[#C3B1E1] text-white p-2 rounded-full fixed bottom-7 right-7`}
+              onClick={scrollToTop}
+            >
+              <FaArrowUp />
+            </button>
+          )}
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Footer;
