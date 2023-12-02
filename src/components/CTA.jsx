2a9f28f8f5f9ea9042753f78a8e52cdc5e8a9727
Omar ♔ <o_yousaf@live.co.uk>
2023-12-02 14:06:16 +0000

    squashing bugs

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,9 +6,11 @@ const CTA = () => (
     className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
   >
     <div className="flex-1 flex flex-col">
-      <h2 className={styles.heading2}>Book with us now!</h2>
+      <h2 className={styles.heading2}>Embark on a spectacle safari with us!</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        You can call us anytime between 8:30am-5:30pm Mon-Sat to book an appointment.
+        We await your call between the refined hours of 8:30 am and 5:30 pm,
+        Monday through Saturday. Let's frame your elegance with a touch of
+        optical finesse.
       </p>
     </div>
     <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
