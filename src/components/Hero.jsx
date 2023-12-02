57de1055496b77cf6dcbd7f129e1570d296f200d
Omar ♔ <o_yousaf@live.co.uk>
2023-12-02 13:59:28 +0000

    squashing bugs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,18 @@ import { discount, mrpollard } from "../assets";
 import Explore from "./Explore";
 
 const Hero = () => {
-  // Use useEffect to preload the mrpollard image
   useEffect(() => {
-    const img = new Image();
-    img.src = mrpollard;
+    // Preload LCP Image
+    const link = document.createElement("link");
+    link.rel = "preload";
+    link.as = "image";
+    link.href = "/assets/mrpollard-1015638e.webp";
+    document.head.appendChild(link);
+
+    return () => {
+      // Cleanup: remove the link when the component unmounts
+      document.head.removeChild(link);
+    };
   }, []);
 
   return (
@@ -15,7 +23,9 @@ const Hero = () => {
       id="home"
       className={`flex md:flex-row flex-col ${styles.paddingY} mt-[100px]`}
     >
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+      <div
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+      >
         <div className="flex flex-row items-center py-[6px] px-4 bg-[#C3B1E1] rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
@@ -38,7 +48,9 @@ const Hero = () => {
         </p>
       </div>
 
-      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+      <div
+        className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+      >
         <img
           src={mrpollard}
           alt="mrpollard"
