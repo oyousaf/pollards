b60eeb19d89943b9ed8ee1d810c756068cc66150
Omar ♔ <o_yousaf@live.co.uk>
2023-12-02 12:54:40 +0000

    squashing bugs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,51 +1,71 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "../style";
 import { discount, mrpollard } from "../assets";
 import Explore from "./Explore";
 
-const Hero = () => (
-  <section
-    id="home"
-    className={`flex md:flex-row flex-col ${styles.paddingY} mt-[100px]`}
-  >
-    <div
-      className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+const Hero = () => {
+  // useEffect to preload the mrpollard image
+  useEffect(() => {
+    const img = new Image();
+    img.src = mrpollard;
+    // Event listener to handle the 'load' event
+    img.addEventListener("load", () => {
+      console.log("mrpollard image preloaded");
+    });
+    // Handle the 'error' event
+    img.addEventListener("error", (error) => {
+      console.error("Error preloading mrpollard image:", error);
+    });
+
+    // Cleanup: Remove event listeners when the component unmounts
+    return () => {
+      img.removeEventListener("load", () => {});
+      img.removeEventListener("error", () => {});
+    };
+  }, []);
+
+  return (
+    <section
+      id="home"
+      className={`flex md:flex-row flex-col ${styles.paddingY} mt-[100px]`}
     >
-      <div className="flex flex-row items-center py-[6px] px-4 bg-[#C3B1E1] rounded-[10px] mb-2">
-        <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-        <p className={`${styles.paragraph} ml-2`}>
-          <span className="text-[#002D04]">
-            Providing innovative eye care and an exemplary service for all
-          </span>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row items-center py-[6px] px-4 bg-[#C3B1E1] rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-[#002D04]">
+              Providing innovative eye care and an exemplary service for all
+            </span>
+          </p>
+        </div>
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-gray-300 ss-leading-[100px] leading-[72px]">
+            Pollards Opticians
+            <br className="sm:block hidden" />{" "}
+          </h1>
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <Explore />
+          </div>
+        </div>
+        <p className={`${styles.paragraph} max-w-[570px] mt-5 text-white`}>
+          Visionaries see beyond the boundaries of eyesight
         </p>
       </div>
-      <div className="flex flex-row justify-between items-center w-full">
-        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-gray-300 ss-leading-[100px] leading-[72px]">
-          Pollards Opticians
-          <br className="sm:block hidden" />{" "}
-        </h1>
-        <div className="ss:flex hidden md:mr-4 mr-0">
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img
+          src={mrpollard}
+          alt="mrpollard"
+          width={200}
+          height={100}
+          className="md:w-[400px] z-[5] relative rounded-xl mr-3"
+        />
+        <div className={`ss:hidden ${styles.flexCenter}`}>
           <Explore />
         </div>
       </div>
-      <p className={`${styles.paragraph} max-w-[570px] mt-5 text-white`}>
-        Visionaries see beyond the boundaries of eyesight
-      </p>
-    </div>
-
-    <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-      <img
-        src={mrpollard}
-        alt="mrpollard"
-        width={200}
-        height={100}
-        className="md:w-[400px] z-[5] relative rounded-xl mr-3"
-      />
-      <div className={`ss:hidden ${styles.flexCenter}`}>
-        <Explore />
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Hero;
