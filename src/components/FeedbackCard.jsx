47278b9265dac0d42024167bd36b1397fd58c47e
Omar ♔ <o_yousaf@live.co.uk>
2023-12-02 12:05:50 +0000

    squashing bugs

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -14,9 +14,9 @@ const FeedbackCard = ({ content, name }) => (
     </p>
     <div className="flex flex-row">
       <div className="flex flex-col ml-4">
-        <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">
+        <h2 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">
           {name}
-        </h4>
+        </h2>
       </div>
     </div>
   </div>
