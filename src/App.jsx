3e8fb833e5ceb633dec7beefdd83d9c8428e20e1
Omar ♔ <o_yousaf@live.co.uk>
2023-12-27 14:25:14 +0000

    implementing snowflakes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import Snowfall from "react-snowfall";
+
 import styles from "./style";
 import {
   Nav,
@@ -20,6 +22,10 @@ import { reportWebVitals } from "./reportWebVitals";
 const App = () => (
   <Router>
     <div className="bg-primary w-full overflow-hidden">
+      <Snowfall
+        snowflakeCount={100}
+        style={{ bottom: 0, position: "fixed", width: "100%" }}
+      />
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Nav />
