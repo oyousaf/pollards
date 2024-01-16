25b4ea9d84eef4e7093e2a24090a570e05874c1a
Omar ♔ <o_yousaf@live.co.uk>
2024-01-16 17:01:54 +0000

    squashing bugs

--- src/components/Endorsements.jsx
+++ src/components/Endorsements.jsx
@@ -7,7 +7,7 @@ const Endorsements = () => (
     id="review"
     className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
   >
-    <div className="absolute z-[0] w-[60%] h-[60%] "></div>
+    <div className="absolute z-[0] w-[60%] h-[60%]"></div>
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
       <h1 className={styles.heading2}>Reviews</h1>
       <br className="sm:block hidden" />
